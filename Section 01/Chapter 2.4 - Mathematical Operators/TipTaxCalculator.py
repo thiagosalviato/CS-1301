04b4442bed853890a6fd4bc7e85bd4acb8f4afcf
thiagosalviato <thiago_salviato@outlook.com>
2022-09-28 18:21:23 -0300

    Update Chapter 2.4

---
+++ Section 01/Chapter 2.4 - Mathematical Operators/TipTaxCalculator.py
@@ -0,0 +1,9 @@
+meal_cost = 10.00
+tax_rate = 0.08
+tip_rate = 0.2
+
+Subtotal = meal_cost
+Tax= tax_rate*10
+Tip = tip_rate*100
+Total = Subtotal + Tax + Tip
+print(Total)
---
+++ Section 01/Chapter 2.4 - Mathematical Operators/test.py
@@ -0,0 +1,9 @@
+my_var_1 = 7
+my_var_2 = 4
+my_var_1 //= my_var_2
+my_var_2 **= my_var_1
+my_var_2 %= my_var_1
+my_var_1 *= my_var_2
+
+print(my_var_1)
+print(my_var_2)
